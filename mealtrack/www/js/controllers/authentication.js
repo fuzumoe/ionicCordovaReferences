26e8e4d16e8fc2916ed455cadb024e0b5a784391
Adam <fuzumoe@gmail.com>
2016-10-06 23:41:10 +0200

    ...  intitial

--- mealtrack/www/js/controllers/authentication.js
+++ mealtrack/www/js/controllers/authentication.js
@@ -3,34 +3,42 @@ var app = angular.module('mealtrack.controllers.authentication', []);
 /*********************************************************************
  * LoginCtrl
  *********************************************************************/
-app.controller('LoginCtrl', function ($scope, $state, AuthService) {
+app.controller('LoginCtrl', function($scope, $state, AuthService) {
 
-	$scope.formData = {
-		"email": "",
-		"password": ""
-	};
+    $scope.formData = {
+        "email": "",
+        "password": ""
+    };
+    $scope.form;
 
-	$scope.login = function () {
-		console.log("LoginCtrl::login");
-		//TODO
-	};
+    $scope.login = function(form) {
+        console.log(form);
+        $scope.form = form;
+        console.log("LoginCtrl::login");
+        if (form.$valid) {
+
+        } else {
+            console.log("ERROR");
+        }
+        //TODO
+    };
 
 });
 
 /*********************************************************************
  * SignupCtrl
  *********************************************************************/
-app.controller('SignupCtrl', function ($scope, $state, AuthService) {
+app.controller('SignupCtrl', function($scope, $state, AuthService) {
 
-	$scope.formData = {
-		"name": "",
-		"email": "",
-		"password": ""
-	};
+    $scope.formData = {
+        "name": "",
+        "email": "",
+        "password": ""
+    };
 
-	$scope.signup = function () {
-		console.log("SignupCtrl::signup");
-		//TODO
-	};
+    $scope.signup = function() {
+        console.log("SignupCtrl::signup");
+        //TODO
+    };
 
 });
