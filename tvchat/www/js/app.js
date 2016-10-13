5c0ebb8491879e2c28db19fde7a1a229a8d0be76
Adam <fuzumoe@gmail.com>
2016-10-13 12:19:29 +0200

    firt commit

--- tvchat/www/js/app.js
+++ tvchat/www/js/app.js
@@ -1,80 +1,79 @@
 var app = angular.module('tvchat', [
-	'ionic',
-	'ngCordova',
-	'ngOpenFB',
-	'firebase',
-	'angularMoment',
-	'tvchat.controllers',
-	'tvchat.services',
-	'tvchat.filters',
-	'tvchat.utils'
+    'ionic',
+    'ngCordova',
+    'ngOpenFB',
+    'firebase',
+    'angularMoment',
+    'tvchat.controllers',
+    'tvchat.services',
+    'tvchat.filters',
+    'tvchat.utils'
 ]);
 
 
-app.constant("FACEBOOK_APP_ID", '<REPLACE-ME>');
+app.constant("FACEBOOK_APP_ID", '1785536888369508');
+app.constant("FIREBASE_URL", 'https://tvchat-prod-956fa.firebaseio.com');
+
+app.run(function($rootScope, $ionicPlatform, $cordovaStatusbar) {
 
 
-app.run(function ($rootScope, $ionicPlatform, $cordovaStatusbar) {
+    $ionicPlatform.ready(function() {
 
-
-		$ionicPlatform.ready(function () {
-
-			// Hide the accessory bar by default
-			if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-				cordova.plugins.Keyboard.disableScroll(true);
-			}
-			// Color the iOS status bar text to white
-			if (window.StatusBar) {
-				$cordovaStatusbar.overlaysWebView(true);
-				$cordovaStatusbar.style(0); //Light
-			}
-		});
-	});
-
-app.run(function (ngFB, FACEBOOK_APP_ID) {
-	ngFB.init({appId: FACEBOOK_APP_ID});
+        // Hide the accessory bar by default
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        // Color the iOS status bar text to white
+        if (window.StatusBar) {
+            $cordovaStatusbar.overlaysWebView(true);
+            $cordovaStatusbar.style(0); //Light
+        }
+    });
 });
 
-app.config(function ($stateProvider, $urlRouterProvider) {
+app.run(function(ngFB, FACEBOOK_APP_ID) {
+    ngFB.init({ appId: FACEBOOK_APP_ID });
+});
 
-		$stateProvider
-			.state('intro', {
-				url: '/',
-				templateUrl: 'templates/intro.html',
-				controller: 'IntroCtrl'
-			})
+app.config(function($stateProvider, $urlRouterProvider) {
 
-			.state('app', {
-				url: "/app",
-				abstract: true,
-				templateUrl: "templates/menu.html",
-				controller: 'MenuCtrl'
-			})
+    $stateProvider
+        .state('intro', {
+            url: '/',
+            templateUrl: 'templates/intro.html',
+            controller: 'IntroCtrl'
+        })
 
-			.state('app.search', {
-				url: "/search",
-				views: {
-					'menuContent': {
-						templateUrl: "templates/search.html",
-						controller: 'SearchCtrl'
-					}
-				}
-			})
+    .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'MenuCtrl'
+    })
 
-			.state('app.show', {
-				url: "/show/:showId",
-				cache: false,
-				views: {
-					'menuContent': {
-						templateUrl: "templates/show.html",
-						controller: 'ShowCtrl'
-					}
-				}
-			})
-		;
+    .state('app.search', {
+        url: "/search",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/search.html",
+                controller: 'SearchCtrl'
+            }
+        }
+    })
 
-		// if none of the above states are matched, use this as the fallback
-		$urlRouterProvider.otherwise('/');
+    .state('app.show', {
+        url: "/show/:showId",
+        cache: false,
+        views: {
+            'menuContent': {
+                templateUrl: "templates/show.html",
+                controller: 'ShowCtrl'
+            }
+        }
+    });
 
-	});
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/');
+
+});
